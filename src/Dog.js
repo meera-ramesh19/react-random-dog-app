50a166ca4463237680473681e8d3e12670b7e8fb
Myra Smith <myra.fowlkes.smith@gmail.com>
2022-05-25 10:40:04 -0400

    initial commit

---
+++ src/Dog.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+class Dog extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <article>
+        <p>IMAGE PLACEHOLDER</p>
+        <button>Load new Dog</button>
+      </article>
+    );
+  }
+}
+
+export default Dog;
---
+++ src/NumberOfDogs.js
@@ -0,0 +1,13 @@
+function NumberOfDogs(props) {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="numberOfDogs">How Many Dogs Would you like?</label>
+        <input type="number" name="numberOfDogs" id="" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default NumberOfDogs;
